a3fa6c472fefe79d2fdd26ac78d32749367b3886
Axel-Correa <163211476+Axel-Correa@users.noreply.github.com>
2024-07-18 19:08:29 -0300

    Proyecto-Final

--- js/turnos.js
+++ js/turnos.js
@@ -3,6 +3,7 @@ let miFormulario = document.getElementById("formu");
 const mainFormulario = document.getElementById("mainFormulario");
 const divFormu = document.createElement("div");
 const h4Formu = document.createElement("h4");
+const boton = document.getElementById("botonEnviar")
 
 miFormulario.appendChild(divFormu);
 
@@ -17,114 +18,29 @@ mainFormulario.addEventListener('submit', (e) => {
     localStorage.setItem('modelo', modelo);
     localStorage.setItem('servicio', servicioFomr);
     localStorage.setItem('fecha', fechaFormr);
-    h4Formu.innerText = `Turno reservado el día de este mes ${fechaFormr} por: ${nombre}. El servicio a realizar para el vehículo ${modelo} es: ${servicioFomr}`;
+    setTimeout(()=>{
+        h4Formu.innerText = 'Turno reservado el día' ${fechaFormr} 'de este mes  por:' ${nombre}'El servicio a realizar para el vehículo'  ${modelo} 'es:' ${servicioFomr};
+    } , 2500)
     divFormu.appendChild(h4Formu);
 });
 
+boton.addEventListener ('click',() =>{
+    Swal.fire({
+        title: "Excelente!",
+        Text: "Sus datos sean envia correctamente!",
+        icon: "success",
+    });
+}) 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-1ro: hacer que usuario coloque su nombre y apellido, DNI.
-3ro: Coloque su modelo de vehiculo y años.
-2ro: que coloque el dia a reservar el turno (Mes-dia "1-30").
-/*
-1ro: hacer que usuario coloque su nombre y apellido, DNI.
-3ro: Coloque su modelo de vehiculo y años.
-2ro: que coloque el dia a reservar el turno (Mes-dia "1-30").
-4to: si el turno esta disponible que devuelva: se agendo el turno con el dia, el nombre y el auto.
-    si el turno esta ocupado que devuelva: que esta ocupado el turno.
-
-Variables: cantidad de turnos dispobibles
-Ciclo: que se vaya restando la cantidad de turnos que alla (ejemplo 30 turnos al mes) hasta llegar a 0 turnos.
-Condicionales: si el turno que selecciono el usuario ya esta reservado que no agende el turno.
-
-
-//variable 
-let turnos= 1
-let turnoLimite= 0;
-
-let nombreCompleto = "";
-let tipoDeVehiculo = "";
-let servicioARealizar = "";
-let fecha = "";
-let auto = "";
-let servicio = "";
-let dia = ""; 
-let turnoProximo = "";
-
-
-//alert con los datos para sacar el turno/ 
-function botonTurno(){
-    while(turnos>=turnoLimite){
-        nombreCompleto = prompt ("C");
-        while(nombreCompleto === "" || nombreCompleto == null){
-            nombreCompleto = prompt("ingrese su nombre completo");
-        }
-        tipoDeVehiculo = prompt ("ingrese el modelo de su vehiculo y el año");
-        while(tipoDeVehiculo === "" || tipoDeVehiculo == null){
-            tipoDeVehiculo = prompt("ingrese el modelo de su vehiculo y el año");
-        }
-        servicioARealizar = prompt("ingrese el servico a realizar");
-        while (servicioARealizar === "" || servicioARealizar == null){
-            servicioARealizar = prompt("ingrese el servico a realizar");
-        }
-        fecha = prompt("Ingrese la fecha del Turno");
-        while(fecha === "" || fecha == null){
-            fecha = prompt("ingrese la fecha del Turno ")
-        }
-        alert ("Su turno se reservo con exito");
-        turnos--
-        if (turnoLimite === turnos){
-        break // se rompe el bucle cuando turnosLimites sae estrictamente igual a turnos
-        }
-    }
-    console.log ("Nombre completo:" + nombreCompleto, "Tipo de vehiculo:" + tipoDeVehiculo, "Servicio a Realizar:" + servicioARealizar, "Fecha del turno:" + fecha );
-    const turn =[];
-    /alert No quedan turnos/
-    function turnosdisponible (turnoLimite, turnos){
-        if (turnoLimite === turnos) {
-            alert("Ya no quedan turnos disponibles")
-        }
-    }
-    
-}
-
-/*codigo del bt */
-document.getElementById("startButton").addEventListener("click", botonTurno);
-
-
-
-/*function turnoProximo = new turnoProximos (nombreCompleto, tipoDeVehiculo, servicioARealizar, fecha);
-    turn.push(new turnoProximos);
-    console.log(turn);
-    console.log(turnoProximo);
-    */ 
-/*
-    function turnoProximos (auto, servicio, dia){
-        this.auto = auto.toUppercase();
-        this.servicio = servicio.toUppercase();
-        this.dia = parseInt (dia);
-    }*/
+fetch ("file:///C:/Users/Usuario/Downloads/COM-57705--main/turnos.html")
+    .then((response) => response.json())
+    .then((data) => {
+        data.forEach(element => {
+        const li = document.createElement('li');
+        li.innerHTML =
+                            <><h3>${element.nombre}</h3><h4>${element.modelo}</h4><h5>${element.servicioFomr}</h5><h6>${element.fechaFormr}</h6></>
+                        
+                            
+            lista.appendChild(li);
+        });
+})
